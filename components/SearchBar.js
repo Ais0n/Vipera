52b79541bd891ac3335c8c52c12a65c8c2bd262d
chloeg12 <chloegrh12@outlook.com>
2024-02-06 01:09:54 -0500

    generate images work with API response

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,31 +1,42 @@
+// components/SearchBar.js
+
+import React, { useState } from 'react';
 import styles from '../styles/SearchBar.module.css';
 
-function SearchBar() {
-    return (
-      <div className={styles.searchContainer}> {/* This container should be positioned relative */}
-        
-        <label className={styles.generateLabel}>Generate</label>
-        
-        <input
-            type="text"
-            className={styles.searchBar}
-            placeholder="Write prompt here"
-        />
+function SearchBar({ onGenerateClick, isGenerating }) {
+  const [inputValue, setInputValue] = useState('');
 
-        <div className={styles.searchButtonContainer}>
-            <button className={styles.searchButton}>
-                <span className={styles.searchButtonText}>Generate</span>
-            </button>
-        </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onGenerateClick(inputValue);
+  };
 
+  return (
+    <div className={styles.searchContainer}>
+        <form onSubmit={handleSubmit}>
+            <label className={styles.generateLabel}>Generate</label>
+            <input
+                type="text"
+                className={styles.searchBar}
+                placeholder="Write prompt here"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                disabled={isGenerating}
+            />
+            <div className={styles.searchButtonContainer}>
+                <button type="submit" className={styles.searchButton} disabled={isGenerating}>
+                    <span className={styles.searchButtonText}>Generate</span>
+                </button>
+            </div>
+        </form>
         <div className={styles.sdButtonContainer}>
             <button className={styles.sdButton}>
-                <span className={styles.sdButtonText}>Stable Diffusion</span>
+            <span className={styles.sdButtonText}>Stable Diffusion</span>
             </button>
         </div>
     </div>
-        
-    );
+  );
 }
 
 export default SearchBar;
+
