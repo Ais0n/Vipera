ace116956dfc2d0dedbead921355ad500cef26cd
chloeg12 <chloegrh12@outlook.com>
2024-02-10 02:33:58 -0500

    [Feature] change all styles to flexbox

---
+++ components/Analyze.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Category from './CategoryButton';
+import ImageGrid from './ImageGrid';
+import Distributions from './Distributions';
+import Discussion from './Discussion';
+
+import style from '../styles/Analyze.module.css';
+
+const Analyze = ({ images, distribution, selectedCategory, onSelectCategory }) => {
+    return (
+        <section className={style.analyzeSection}>
+            <div className={style.greyBg}>
+                <div className={style.analyzeContent}>
+                    <div className={style.analyzeText}>Analyze</div>
+                    <Category onSelectCategory={onSelectCategory} selectedCategory={selectedCategory} />
+                    {selectedCategory === 'images' ? (
+                    <ImageGrid images={images} />
+                    ) : (
+                    <Distributions distribution={distribution} category={selectedCategory} />
+                    )}
+                </div>
+                <div className={style.discussionContainer}>
+                    <Discussion />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Analyze;
--- pages/generate.js
+++ pages/generate.js
@@ -2,11 +2,9 @@
 
 import React, { useState } from 'react';
 import SearchBar from '../components/SearchBar';
-import ImageGrid from '../components/ImageGrid';
-import ImageStyle from '../styles/ImageGrid.module.css';
-import Distributions from '../components/Distributions';
-import Category from '../components/CategoryButton';
-import Discussion from '../components/Discussion';
+import Analyze from '../components/Analyze';
+
+import AnalyzeStyle from '../styles/Analyze.module.css';
 
 const Generate = () => {
   const [isGenerating, setIsGenerating] = useState(false);
@@ -63,32 +61,24 @@ const Generate = () => {
     }
   };
 
-  const loadingGifPath = '/loading_image1.gif';
-
   return (
     <div>
       <SearchBar onGenerateClick={handleGenerateClick} isGenerating={isGenerating} />
       {error && <p>{error}</p>}
       {isGenerating ? (
-        <div className={ImageStyle.loadingContainer}>
-          <img src={loadingGifPath} alt="LoadingGIF" />
+        <div className={AnalyzeStyle.loadingContainer}>
+          <img src={'/loading_image1.gif'} alt="LoadingGIF" />
         </div>
       ) : (
         images.length > 0 && (
-          <section className={ImageStyle.analyzeSection}>
-            <div className={ImageStyle.greyBg} style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div>
-                <div className={ImageStyle.analyzeText}>Analyze</div>
-                <Category onSelectCategory={setSelectedCategory} selectedCategory={selectedCategory} />
-                {selectedCategory === 'images' ? (
-                  <ImageGrid images={images} />
-                ) : (
-                  <Distributions distribution={distribution} category={selectedCategory}/>
-                )}
-              </div>
-              <Discussion />
-            </div>
-          </section>
+          <>
+            <Analyze
+              images={images}
+              distribution={distribution}
+              selectedCategory={selectedCategory}
+              onSelectCategory={setSelectedCategory}
+            />
+          </>
         )
       )}
     </div>
