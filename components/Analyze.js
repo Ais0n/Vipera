ee81038fdb5b3fbdeacd51dd5b24adfe432ddafa
chloeg12 <chloegrh12@outlook.com>
2024-02-17 16:51:05 -0500

    [Feature] change invoke the thread from analyze to discussion component

--- components/Analyze.js
+++ components/Analyze.js
@@ -6,21 +6,10 @@ import Category from './CategoryButton';
 import ImageGrid from './ImageGrid';
 import Distributions from './Distributions';
 import Discussion from './Discussion';
-import Thread from './Thread';
 
 import style from '../styles/Analyze.module.css';
-import threadStyle from '../styles/Thread.module.css';
 
 const Analyze = ({ images, distribution, selectedCategory, onSelectCategory }) => {
-
-    const [isThreadVisible, setThreadVisible] = useState(false);
-
-    // click "Start a discussion" button to show the thread
-    const handleStartDiscussion = () => setThreadVisible(true);
-
-    // click '-' to close the thread
-    const handleCloseThread = () => setThreadVisible(false);
-
     return (
         <section className={style.analyzeSection}>
             <div className={style.greyBg}>
@@ -35,13 +24,9 @@ const Analyze = ({ images, distribution, selectedCategory, onSelectCategory }) =
                 </div>
 
                 <div className={style.discussionContainer}>
-                    <Discussion onStartDiscussion={handleStartDiscussion} />
+                    <Discussion />
                 </div>
-
-                {isThreadVisible && (
-                    <Thread onCloseThread={handleCloseThread} />
-                )}
-        </div>
+            </div>
         </section>
     );
 };
--- components/Discussion.js
+++ components/Discussion.js
@@ -1,16 +1,25 @@
 // components/Discussion.js
 
-import React from 'react';
+import React, { useState } from 'react';
+import Thread from './Thread';
 import styles from '../styles/Discussion.module.css';
 
-const Discussion = ({ onStartDiscussion }) => {
+const Discussion = () => {
+  const [isThreadVisible, setThreadVisible] = useState(false);
+
+  const handleStartDiscussion = () => setThreadVisible(true);
+  const handleCloseThread = () => setThreadVisible(false);
+
   return (
     <div className={styles.discussionContainer}>
       <h3 className={styles.discussionTitle}>Do you find the generated image skewed in any way?</h3>
       <p className={styles.discussionSubtitle}>Images generated may contain algorithmic bias. Discussions help bring awareness.</p>
-      <button className={styles.discussionButton} onClick={onStartDiscussion}>
+      <button className={styles.discussionButton} onClick={handleStartDiscussion}>
         Start a discussion
       </button>
+      {isThreadVisible && (
+        <Thread onCloseThread={handleCloseThread} />
+      )}
     </div>
   );
 };
