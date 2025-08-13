f2878be1cc37e581ea25a29fba6a71d6a27556ad
yanwei <huangyw@zju.edu.cn>
2025-08-14 01:43:15 +0800

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
         MODEL_API_KEY: process.env.MODEL_API_KEY,
     },
     reactStrictMode: false,
+    basePath: process.env.NODE_ENV === 'production' ? '/demo1' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/demo1' : '',
     transpilePackages: [
         "antd",
         "rc-util",
